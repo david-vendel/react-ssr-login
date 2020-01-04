f3d0c9bee0caf672a3c6650f7329500d42f2ddf9
David Vendel <33971649+justdvl@users.noreply.github.com>
2020-01-04 18:26:09 +0100

    helmet added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOMServer from "react-dom/server";
 import { StaticRouter } from "react-router";
 import bodyParser from "body-parser";
-
+import { Helmet } from "react-helmet";
 import App from "./src/app";
 
 const app = express();
@@ -22,9 +22,13 @@ app.get("*", (req, res) => {
     </StaticRouter>
   );
 
+  const helmet = Helmet.renderStatic();
+
   const html = `
     <html>
         <head>
+            ${helmet.meta.toString()}
+            ${helmet.title.toString()}
         </head>
         <body>
             <div id="root">
--- src/pages/homePageComponent.js
+++ src/pages/homePageComponent.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 class Home extends React.Component {
   exampleMethod() {
     console.log("JS is running");
   }
 
+  head() {
+    return (
+      <Helmet>
+        <title>My page title - justDVL</title>
+      </Helmet>
+    );
+  }
+
   render() {
     return (
       <div>
+        {this.head()}
         <h1>My home page</h1>
         <p>Some content..</p>
         <button onClick={() => this.exampleMethod()}>Console log</button>
